712939a3b30c6acb68fd0817d636807f830b9045
Aleksey1706 <aleksey17061@gmail.com>
2018-02-14 13:40:40 +0200

    add maim page,for admin panel

--- resources/views/AdminPanel/main.blade.php
+++ resources/views/AdminPanel/main.blade.php
@@ -36,6 +36,9 @@
                 <li class="nav-item">
                     <a class="nav-link " href="{{ route('home') }}">Вернуться в блог</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="{{ route('admin') }}">Админ панель</a>
+                </li>
 
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('addPost') }}">Добавить пост</a>

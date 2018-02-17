1fe3a5c9c59ade98539bc12c461c3b0b7cc525d5
Aleksey1706 <aleksey17061@gmail.com>
2018-02-17 11:58:15 +0200

    admin user name fix1

--- resources/views/AdminPanel/all-comments.blade.php
+++ resources/views/AdminPanel/all-comments.blade.php
@@ -8,7 +8,7 @@
                 @foreach($comments as $comment)
                     <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4">
                         <div class="news-item">
-                            <h1 class="header-home-page"><p>{{ $comment->user->name }} = {{ $comment->user_id }}</p></h1>
+                            <h1 class="header-home-page"><p>{{ $comment->user->name }}</p></h1>
                             <p class="text-justify">{{ $comment->comment }}</p>
                             <a class="btn btn-info btn-sm" role="button"
                                href="{{ route('editComment',['id' => $comment->id]) }}">Редактировать коментарий</a>
